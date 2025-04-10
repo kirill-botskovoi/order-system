dbe0d2c65edbf7d0101a1a5947f3125c751ca0e8
kirill-botskovoi <kirill.botskovoi@gmail.com>
2025-04-10 18:01:10 +0300

    testing services with kafka. bug appeared

--- product-service/src/main/java/org/kbotsk/product/Service/ProductService.java
+++ product-service/src/main/java/org/kbotsk/product/Service/ProductService.java
@@ -1,8 +1,10 @@
 package org.kbotsk.product.Service;
 
 import lombok.RequiredArgsConstructor;
+import org.kbotsk.product.Dto.ProductCreatedEvent;
 import org.kbotsk.product.Entity.Product;
 import org.kbotsk.product.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ProductEventPublisher eventPublisher;
 
     public List<Product> getAll() {
         return productRepository.findAll();
@@ -23,6 +26,9 @@ public class ProductService {
     }
 
     public Product create(Product product) {
+        eventPublisher.sendProductCreatedEvent(
+                new ProductCreatedEvent(product.getId(), product.getName(), product.getPrice())
+        );
         return productRepository.save(product);
     }
 

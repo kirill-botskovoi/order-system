8775d3bbc7f2d144227f8f3f7b0904336cda4e83
kirill-botskovoi <kirill.botskovoi@gmail.com>
2025-04-10 03:35:52 +0300

    added auth-service with Spring Security + JWT

---
+++ auth-service/src/main/java/org/kbotsk/auth/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.kbotsk.auth.Repository;
+
+import org.kbotsk.auth.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
